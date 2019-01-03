ed235e8d3e3ba1d203e60f984021c413cc753ca3
Piet de Vries <p.h.devries@gmail.com>
2019-01-03 17:13:24 +0100

    delete unused client js gsignin/out

--- client/public/javascripts/script.js
+++ client/public/javascripts/script.js
@@ -1,25 +1,5 @@
-let profile;
 
-function onSignIn(googleUser) {
-    profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    $(".g-signin2").hide();
-    $("#profile-pic").attr('src', profile.getImageUrl());
-    $("#signoutsection").show();
-}
-
-function signOut() {
-    let auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-        profile = null;
-        $(".g-signin2").show();
-        $("#signoutsection").hide();
-    });
-}
+// PDV: outcommented because feature hasn't been developed and gives annoying errors
 // $(() => {
 //     $("#send").click(()=>{
 //        sendMessage({
